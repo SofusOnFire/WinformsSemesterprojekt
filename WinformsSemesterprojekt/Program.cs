83145f10617ea2a128925098dd38fe44da775ade
LuffeDeluXe <luffedeluxe@gmail.com>
2024-12-05 14:36:36 +0100

    rewgnjlksdfgbj

--- WinformsSemesterprojekt/Program.cs
+++ WinformsSemesterprojekt/Program.cs
@@ -17,6 +17,8 @@ namespace WinformsSemesterprojekt
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            //Hej :3 :P
         }
     }
 }
